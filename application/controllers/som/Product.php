e5059aad7047d2c96ab1d80c1841be5a1bb9ed15
G-Desktop <laparkaes@gmail.com>
2024-04-08 23:58:56 -0500

    .

---
+++ application/controllers/som/Product.php
@@ -0,0 +1,24 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+use PhpOffice\PhpSpreadsheet\IOFactory;
+
+class Product extends CI_Controller {
+
+	public function __construct(){
+		parent::__construct();
+		if (!$this->session->userdata('logged_in')) redirect("/auth/login");
+		
+		date_default_timezone_set('America/Lima');
+		$this->load->model('general_model', 'gen_m');
+	}
+	
+	public function index(){
+		$data = [
+			"purchase_order_temps" => $this->gen_m->all("product", [["category_id", "asc"], ["model", "asc"]]),
+			"main" => "som/product/index",
+		];
+		
+		$this->load->view('layout', $data);
+	}
+}
--- application/views/dashboard/index.php
+++ application/views/dashboard/index.php
@@ -23,11 +23,13 @@
 					</div>
 				</div>
 			</div>
+		</div>
+		<div class="col-md-4">
 			<div class="card">
 				<div class="card-body pb-0">
 					<h5 class="card-title">SCM - Supply Chain Management</h5>
 					<div class="row">
-						<div class="col-md-4">
+						<div class="col-md-12">
 							<div class="card info-card sales-card pb-0">
 								<div class="card-body p-3">
 									<div class="d-flex align-items-center">
@@ -45,6 +47,47 @@
 					</div>
 				</div>
 			</div>
+		</div>
+		<div class="col-md-8">
+			<div class="card">
+				<div class="card-body pb-0">
+					<h5 class="card-title">SOM - Sales Order Management</h5>
+					<div class="row">
+						<div class="col-md-6">
+							<div class="card info-card sales-card pb-0">
+								<div class="card-body p-3">
+									<div class="d-flex align-items-center">
+										<div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+											<i class="bi bi-box-seam"></i>
+										</div>
+										<div class="ps-3">
+											<a href="<?= base_url() ?>som/product"><h6>Product</h6></a>
+											<span class="text-muted small pt-2">Admin product's information</span>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-6">
+							<div class="card info-card sales-card pb-0">
+								<div class="card-body p-3">
+									<div class="d-flex align-items-center">
+										<div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+											<i class="bi bi-arrow-left-right"></i>
+										</div>
+										<div class="ps-3">
+											<a href="<?= base_url() ?>som/sku"><h6>SKU</h6></a>
+											<span class="text-muted small pt-2">Admin customer SKU information</span>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-12">
 			<div class="card">
 				<div class="card-body pb-0">
 					<h5 class="card-title">HR - Human Resource</h5>
