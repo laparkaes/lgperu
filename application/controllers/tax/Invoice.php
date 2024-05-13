416ec4d3bae037539b0a939f782a0f4482a65dd0
G-Desktop <laparkaes@gmail.com>
2024-05-13 00:17:06 -0500

    .

--- application/controllers/tax/Invoice.php
+++ application/controllers/tax/Invoice.php
@@ -37,7 +37,7 @@ class Invoice extends CI_Controller {
 			$rowdata = $this->my_func->arr_trim($sheet->rangeToArray("A{$row}:{$max_col}{$row}")[0]);
 			
 			if (!array_key_exists($rowdata[1], $invoices)) 
-				$invoices[$rowdata[1]] = [$rowdata[11], $rowdata[0], $rowdata[1]];
+				$invoices[$rowdata[1]] = [$rowdata[0], $rowdata[1], $rowdata[11]];
 		}
 		
 		//GERP process
@@ -45,58 +45,31 @@ class Invoice extends CI_Controller {
 		$max_row = $sheet->getHighestRow();
 		$max_col = $sheet->getHighestColumn();
 		
-		$report = [];
+		$rows = [];
 		$blank_arr = ["", "", ""];
 		
 		for ($row = 2; $row <= $max_row; $row++){
 			$rowdata = $this->my_func->arr_trim($sheet->rangeToArray("A{$row}:{$max_col}{$row}")[0]);
 			
+			//print_r($rowdata); echo "<br/>";
+			
 			$aux = [];
 			if ($rowdata[4]) $aux[] = $rowdata[4];
 			if ($rowdata[5]) $aux[] = $rowdata[5];
 			$inv = implode("-", $aux);
 			
-			$data = [$rowdata[0], $rowdata[1], $rowdata[2], $rowdata[9], $rowdata[10]];
-			$report[] = array_key_exists($inv, $invoices) ? array_merge($invoices[$inv], $data) : array_merge($blank_arr, $data);
-			
-			/*
-			$inv_date = $rowdata[26];
-			
-			$aux = [];
-			if ($rowdata[4]) $aux[] = $rowdata[4];
-			if ($rowdata[5]) $aux[] = $rowdata[5];
-			
-			$inv = implode("-", $aux);
-			if ($inv){
-				$inv_key = $inv."_".$inv_date;
-				
-				if (!array_key_exists($inv_key, $invoices)) $invoices[$inv_key] = ["inv" => $inv, "date" => $inv_date, "g" => null, "p" => null];
-				$invoices[$inv_key]["g"] = $rowdata[7];
-			}
-			*/
+			$data = [$rowdata[7], $rowdata[0], $rowdata[1], date("Y-m-d", strtotime($rowdata[2])), $rowdata[9], $rowdata[10], $rowdata[13], $rowdata[14], $rowdata[15]];
+			$rows[] = array_key_exists($inv, $invoices) ? array_merge($invoices[$inv], $data) : array_merge($blank_arr, $data);
 		}
 		
-		foreach($report as $num => $r){
-			echo $num." ====> ";
-			print_r($r);
-			echo "<br/>";
-			
-		}
-		echo "<br/><br/>";
+		usort($rows, function($a, $b) {
+			if (!strcmp($a[0], $b[0])) return (strtotime($a[6]) > strtotime($b[6]));
+			else return strcmp($a[0], $b[0]);
+		});
 		
-		echo count($report);
+		$header = ["Document type", "Documment number", "Status (Paperless)", "Status (GERP)", "Voucher_No", "Header_Id", "Date", "Business number", "Business name", "Amount", "VAT", "Total"];
 		
-		//echo $max_row." ".$max_col;
-		
-		/*
-		echo "<br/><br/><br/>";
-		
-		$sheet_p = IOFactory::load("./test_files/tax_e_invoice/paperless 202404.xlsx")->getActiveSheet();
-		
-		$max_row = $sheet_p->getHighestRow();
-		$max_col = $sheet_p->getHighestColumn();
-		
-		echo $max_row." ".$max_col;
-		*/
+		$url = $this->my_func->generate_excel_report("tax_invoice_comparison.xlsx", null, $header, $rows);
+		return $url;
 	}
 }
