2bab606a0d611ef2f426897ef629516de9042011
G-Laptop <laparkaes@gmail.com>
2024-05-25 18:59:57 -0500

    .

--- application/views/dashboard/index.php
+++ application/views/dashboard/index.php
@@ -4,6 +4,10 @@
 			<div class="card">
 				<div class="card-body">
 					<h5 class="card-title">Modules</h5>
+					<div class="form-floating mb-3">
+						<input type="text" class="form-control" id="searchModule" placeholder="Search Module">
+						<label for="searchModule">Search Module</label>
+					</div>
 					<ul class="list-group list-group-flush">
 						<a class="list-group-item list-group-item-action" href="<?= base_url() ?>module/aging">Aging Report</a>
 						<a class="list-group-item list-group-item-action" href="<?= base_url() ?>module/attendance">Attendance</a>
@@ -16,6 +20,16 @@
 						<a class="list-group-item list-group-item-action" href="<?= base_url() ?>module/sales_order">Sales Order</a>
 						<a class="list-group-item list-group-item-action" href="<?= base_url() ?>module/sell_inout">Sell In/Out Report</a>
 						<a class="list-group-item list-group-item-action" href="<?= base_url() ?>module/sell_out">Sell Out</a>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-8">
+			<div class="card">
+				<div class="card-body">
+					<h5 class="card-title">Anything here</h5>
+					<div>
+						Will be developed
 					</div>
 				</div>
 			</div>
