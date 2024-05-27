a87d6e5721e3200db245bd2f4f53c1bbe694ddd7
G-Desktop <laparkaes@gmail.com>
2024-05-27 15:06:31 -0500

    .

--- application/controllers/module/Promotion.php
+++ application/controllers/module/Promotion.php
@@ -23,12 +23,76 @@ class Promotion extends CI_Controller {
 		$this->load->view('layout', $data);
 	}
 	
-	public function search_customer(){
-		$customers = $this->gen_m->filter("customer", null, [["field" => "customer", "values" => [$this->input->post("customer")]]]);
-		print_r($customers);
-	}
-	
-	public function search_product(){
-		print_r($this->input->post());
+	public function test(){
+		$type = "error"; $msg = "";
+		
+		$spreadsheet = IOFactory::load("./test_files/sa_promotion/sa_promotion.xlsx");
+		$sheet = $spreadsheet->getActiveSheet();
+		
+		
+		$h = [
+			trim($sheet->getCell('A1')->getValue()),
+			trim($sheet->getCell('B1')->getValue()),
+			trim($sheet->getCell('C1')->getValue()),
+			trim($sheet->getCell('D1')->getValue()),
+			trim($sheet->getCell('E1')->getValue()),
+			trim($sheet->getCell('F1')->getValue()),
+			trim($sheet->getCell('G1')->getValue()),
+			trim($sheet->getCell('H1')->getValue()),
+			trim($sheet->getCell('I1')->getValue()),
+			trim($sheet->getCell('J1')->getValue()),
+			trim($sheet->getCell('K1')->getValue()),
+			trim($sheet->getCell('L1')->getValue()),
+			trim($sheet->getCell('M1')->getValue()),
+		];
+		
+		$h_origin = [
+			"Seq",
+			"Company Name",
+			"Division Name",
+			"Promotion No",
+			"Promotion Line No",
+			"Fecha Inicio",
+			"Fecha Fin",
+			"Customer Code",
+			"Modelo",
+			"PVP",
+			"Costo Sellin",
+			"* Precio Promotion",
+			"* Nuevo Margen",
+		];
+		
+		$header_validation = true;
+		foreach($h as $i => $h_i) if ($h_i !== $h_origin[$i]) $header_validation = false;
+		
+		$promotions = [];
+		
+		if ($header_validation){
+			$max_row = $sheet->getHighestRow();
+			//$max_col = $sheet->getHighestColumn();
+			
+			for($i = 2; $i < $max_row; $i++){
+				$promotions[] = [
+					"prom" 			=> $sheet->getCell('D'.$i)->getValue(),
+					"prom_line" 	=> $sheet->getCell('E'.$i)->getValue(),
+					"date_start"	=> $sheet->getCell('F'.$i)->getValue(),
+					"date_end"		=> $sheet->getCell('G'.$i)->getValue(),
+					"cus_code"		=> $sheet->getCell('H'.$i)->getValue(),
+					"prod_model"	=> $sheet->getCell('I'.$i)->getValue(),
+					"price_sellin"	=> $sheet->getCell('K'.$i)->getValue(),
+					"prom" 			=> $sheet->getCell('D'.$i)->getValue(),
+					"prom" 			=> $sheet->getCell('D'.$i)->getValue(),
+				];
+				
+				
+				$prom = ;
+				$ = ;
+				
+				echo $prom." ".$prom_line."<br/>";
+			}
+			
+		}else $msg = "Wrong file uploaded.";
+		
+		echo $msg;
 	}
 }
