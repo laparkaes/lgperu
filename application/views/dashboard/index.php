498d1e25036e38cb83629b97c88e0469a694a794
G-Laptop <laparkaes@gmail.com>
2024-06-01 18:55:23 -0500

    .

--- application/views/dashboard/index.php
+++ application/views/dashboard/index.php
@@ -13,13 +13,11 @@
 						<a class="list-group-item list-group-item-action" href="<?= base_url() ?>module/attendance">Attendance</a>
 						<a class="list-group-item list-group-item-action" href="<?= base_url() ?>module/dash_order_inquiry">Dashboard - Order Inquiry</a>
 						<a class="list-group-item list-group-item-action" href="<?= base_url() ?>module/employee">Employee</a>
-						<a class="list-group-item list-group-item-action" href="<?= base_url() ?>module/espr_file">ESPR File (COI, SOI 1, SOI 2)</a>
 						<a class="list-group-item list-group-item-action" href="<?= base_url() ?>module/invoice">Invoice</a>
 						<a class="list-group-item list-group-item-action" href="<?= base_url() ?>module/obs">OBS</a>
 						<a class="list-group-item list-group-item-action" href="<?= base_url() ?>module/product">Product</a>
 						<a class="list-group-item list-group-item-action" href="<?= base_url() ?>module/promotion">Promotion</a>
 						<a class="list-group-item list-group-item-action" href="<?= base_url() ?>module/purchase_order">Purchase Order</a>
-						<a class="list-group-item list-group-item-action" href="<?= base_url() ?>module/sales_order">Sales Order</a>
 						<a class="list-group-item list-group-item-action" href="<?= base_url() ?>module/sell_inout">Sell In/Out Report</a>
 						<a class="list-group-item list-group-item-action" href="<?= base_url() ?>module/sell_out">Sell Out</a>
 					</ul>
--- application/views/module/dash_order_inquiry/index.php
+++ application/views/module/dash_order_inquiry/index.php
@@ -1,6 +1,6 @@
 <section class="section">
 	<div class="row">
-		<div class="col-md-5 mx-auto">
+		<div class="col-md-4 mx-auto">
 			<div class="d-flex justify-content-between align-items-start">
 				<div class="pagetitle">
 					<h1>Dashboard - Order Inquiry</h1>
@@ -28,7 +28,55 @@
 				</div>
 			</div>
 		</div>
-	</div>	
+	</div>
+	<div class="row">
+		<div class="col-md-4 mx-auto">
+			<div class="row">
+				<div class="col-md-6">
+					<div class="card">
+						<div class="card-body">
+							<h5 class="card-title">Sales Order Inquiry</h5>
+							<div class="row g-3">
+								<div class="col-12">
+									<label class="form-label">Last update</label>
+									<input class="form-control" type="text" value="<?= $sales_updated->updated ?>" readonly>
+								</div>
+								<div class="col-12">
+									<label class="form-label">Last Sales Order Date</label>
+									<input class="form-control" type="text" value="<?= $sales_last->order_date ?>" readonly>
+								</div>
+								<div class="col-12">
+									<label class="form-label">First Sales Order Date</label>
+									<input class="form-control" type="text" value="<?= $sales_first->order_date ?>" readonly>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-6">
+					<div class="card">
+						<div class="card-body">
+							<h5 class="card-title">Closed Order Inquiry</h5>
+							<div class="row g-3">
+								<div class="col-12">
+									<label class="form-label">Last update</label>
+									<input class="form-control" type="text" value="<?= $closed_updated->updated ?>" readonly>
+								</div>
+								<div class="col-12">
+									<label class="form-label">Last Closed Order Date</label>
+									<input class="form-control" type="text" value="<?= $closed_last->closed_date ?>" readonly>
+								</div>
+								<div class="col-12">
+									<label class="form-label">First Closed Order Date</label>
+									<input class="form-control" type="text" value="<?= $closed_first->closed_date ?>" readonly>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </section>
 
 <script>
@@ -37,7 +85,7 @@ document.addEventListener("DOMContentLoaded", () => {
 		e.preventDefault();
 		$("#form_upload_order_inquiry .sys_msg").html("");
 		ajax_form_warning(this, "module/dash_order_inquiry/upload", "Do you upload data?").done(function(res) {
-			swal_open_tab(res.type, res.msg, res.url);
+			swal_redirection(res.type, res.msg, "module/dash_order_inquiry");
 		});
 	});
 });
