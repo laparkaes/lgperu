b67c3ed7b21a8d31f8042c5da08ae56c751b0941
G-Desktop <laparkaes@gmail.com>
2024-06-26 00:20:26 -0500

    .

--- db/exchange_rate.sql
+++ db/exchange_rate.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.1.3
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 24, 2024 at 11:33 PM
--- Server version: 10.4.27-MariaDB
--- PHP Version: 7.4.33
+-- 생성 시간: 24-06-26 00:20
+-- 서버 버전: 10.4.24-MariaDB
+-- PHP 버전: 7.4.29
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,50 +18,44 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `llamasys`
+-- 데이터베이스: `llamasys`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `exchange_rate`
+-- 테이블 구조 `exchange_rate`
 --
 
 CREATE TABLE `exchange_rate` (
   `exchange_rate_id` int(11) NOT NULL,
-  `date` date NOT NULL,
+  `date` date DEFAULT NULL,
   `currency_from` varchar(5) NOT NULL,
   `currency_to` varchar(5) NOT NULL,
-  `rate` decimal(10,2) NOT NULL,
-  `valid` tinyint(1) NOT NULL DEFAULT 1
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+  `buy` float NOT NULL,
+  `sell` float NOT NULL,
+  `avg` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `exchange_rate`
---
-
-INSERT INTO `exchange_rate` (`exchange_rate_id`, `date`, `currency_from`, `currency_to`, `rate`, `valid`) VALUES
-(1, '2024-05-01', 'USD', 'PEN', '3.72', 1);
-
---
--- Indexes for dumped tables
+-- 덤프된 테이블의 인덱스
 --
 
 --
--- Indexes for table `exchange_rate`
+-- 테이블의 인덱스 `exchange_rate`
 --
 ALTER TABLE `exchange_rate`
   ADD PRIMARY KEY (`exchange_rate_id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- 덤프된 테이블의 AUTO_INCREMENT
 --
 
 --
--- AUTO_INCREMENT for table `exchange_rate`
+-- 테이블의 AUTO_INCREMENT `exchange_rate`
 --
 ALTER TABLE `exchange_rate`
-  MODIFY `exchange_rate_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `exchange_rate_id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
