66557e7cb83c8cebee00f022d256ec472e603a78
G-Desktop <laparkaes@gmail.com>
2024-07-20 01:58:20 -0500

    .

--- application/controllers/module/Obs_niubiz.php
+++ application/controllers/module/Obs_niubiz.php
@@ -9,48 +9,63 @@ class Obs_niubiz extends CI_Controller {
 	}
 	
 	public function index(){
+		///////////////////////////////////////////////////////////////// Step 0: transaction basic data
+		$clientIp = '24.212.107.30';
+		
+		$card = [
+			//'cardNumber' => '4551708161768059',//with installment
+			'cardNumber' => '4474118355632240',//without installment
+			'expirationMonth' => 3,
+			'expirationYear' => 2028,
+			'cvv2' => '111'
+		];
+		
+		$cardHolder = [
+			'firstName' => 'Pedro',
+			'lastName' => 'Galdamez',
+			'phoneNumber' => '012223333',
+			'email' => 'integraciones@niubiz.com.pe',
+		];//client
+		
+		$order = [
+			'purchaseNumber' => '01234567890',//order number or magento id in our case
+			'currency' => 'PEN',
+			'amount' => 1234.23
+		];
+		
+		$shipping = [
+			'cardholderCity' => 'Lima',
+			'cardholderCountry' => 'PE',
+			'cardholderAddress' => 'Av Jose Pardo 831',
+			'cardholderPostalCode' => '12345',
+			'cardholderState' => 'LIM',
+			'cardholderPhoneNumber' => '987654321'
+		];
+		
 		//load library or make instance of My_niubiz class
 		$this->load->library('my_niubiz');
 		
-		///////////////////////////////////////////////////////////////// Step 1: make access token
-		$accessToken = $this->my_niubiz->getAccessToken();
-		if ($accessToken["success"]){
-			echo "Access token result:<br/>";
-			print_R($accessToken);
-			echo "<br/><br/>=====================<br/><br/>";
-		}else{
-			echo $accessToken["msg"];
-			return;
-		}
-		
-		///////////////////////////////////////////////////////////////// Step 2: make session token
-		$session_token_data = [
+		///////////////////////////////////////////////////////////////// Step 1: session token
+		$data = [
 			'channel' => 'web',
 			'antifraud' => [
 				//you define own business values here
 				'merchantDefineData' => [
-						'orderNo' => '01234567890',
-						'myField1' => 'myValue1',
-						'myField2' => 'myValue2',
-						'myField3' => 'myValue3'
+					'purchaseNumber' => $order['purchaseNumber'],
+					'myField1' => 'myValue1',
+					'myField2' => 'myValue2',
+					'myField3' => 'myValue3'
 				],
-				'clientIp' => '24.212.107.30'
+				'clientIp' => $clientIp,
 			],
-			'dataMap' => [
-				'cardholderCity' => 'Lima',
-				'cardholderCountry' => 'PE',
-				'cardholderAddress' => 'Av Jose Pardo 831',
-				'cardholderPostalCode' => '12345',
-				'cardholderState' => 'LIM',
-				'cardholderPhoneNumber' => '987654321'
-			],
-			'amount' => 123234.34
+			'dataMap' => $shipping,
+			'amount' => $order['amount']
 		];
 		
 		//You can make session token generation error uncommenting below examples
 		//Bad request or no autorized 	=> $accessToken["accessToken"] = "1111111";
 		//Used access token 			=> $accessToken["accessToken"] = "eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICIwTWR3R0R6RjQ1YS1SbWs3bkhwc2lNYUJweFJQRjNzekEtNW1HWFllMThvIn0.eyJleHAiOjE3MjE0MzQ2NzEsImlhdCI6MTcyMTQzMTA3MSwianRpIjoiNmQyYjNjNjgtOGQ5My00MTFhLTg5YzAtNmVlNjNiYjU2ZDI4IiwiaXNzIjoiaHR0cHM6Ly9hY2Nlc3MuaW50dm50LmNvbS9hdXRoL3JlYWxtcy9vbmxpbmUtYXBpcyIsInN1YiI6IjQyNjg5NzZlLWVhOWEtNDI0Yi04YWEwLTY5ZWYwMjA5NTJkZSIsInR5cCI6IkJlYXJlciIsImF6cCI6ImFwcC1tdWx0aXJlZ2lvbiIsInNlc3Npb25fc3RhdGUiOiI3Yzg3YTRjZS1hOGY2LTQyNjMtODc5My04YWE4MDNhNTI2YmMiLCJhY3IiOiIxIiwic2NvcGUiOiJhd3MuY29nbml0by5zaWduaW4udXNlci5hZG1pbiIsImdyb3VwcyI6W10sInVzZXJuYW1lIjoiaW50ZWdyYWNpb25lc0BuaXViaXouY29tLnBlIn0.igIk_sFvENScwjqgfe7BUs_BlExZQdSSshtA6Hd0lY-yUBJHg7Grzll0lHwsp57DV2W9e3NgzRG2sLJKp_X9N8iMWSoaYPT8nlQnkuQvgOajjTjZBg9W-81oWqszwgiW7wwxz2Km75PoowUFPvSgwOkqqZfwy6jeqlcr4q04MTpKpALOa89I-3eRkYi9PHAsmm7HMSfaVTAcNX2eo6rn4UHMCy-_dKy3RkFnvdBoTLmKCASE-gk0GBwLwlBGsrxd4ZRwDpAZ0StCK46GHetkRjL0U1cfrh-ga-v4yaD2Wcc2NOLfZoKOfa_dXlnnIBqtCKQjDalB3vH1vqQl6Jk3bw";
-		$sessionToken = $this->my_niubiz->getSessionToken($accessToken["accessToken"], $session_token_data);
+		$sessionToken = $this->my_niubiz->getSessionToken($data);
 		if ($sessionToken["success"]){
 			echo "Session token result:<br/>"; 
 			print_R($sessionToken); 
@@ -60,79 +75,50 @@ class Obs_niubiz extends CI_Controller {
 			return;
 		}
 		
-		///////////////////////////////////////////////////////////////// Step 3: request autorization
+		///////////////////////////////////////////////////////////////// Step 2: antifraud
+		$data = [
+			'channel' => 'web',
+			'order' => $order,
+			'card' => $card,
+			'cardHolder' => $cardHolder,
+			'clientIp' => $clientIp,
+		];
 		
+		$antifraud = $this->my_niubiz->antifraud($data);
+		if ($antifraud["success"]){
+			echo "Antifraud result:<br/>"; 
+			print_R($antifraud); 
+			echo "<br/><br/>=====================<br/><br/>";
+		}else{
+			echo $antifraud["errorCode"]." - ".$antifraud["errorMessage"];
+			return;
+		}
+		
+		///////////////////////////////////////////////////////////////// Step 3: autorization
+		$order['tokenId'] = $antifraud['token']->tokenId;
+		//$order['externalTransactionId'] = $order['purchaseNumber']."-".$order['purchaseNumber'];
+		$order['installment'] = 12;
+		
+		$data = [
+			'channel' => 'web',
+			'captureType' => 'manual',
+			'countable' => true,
+			'order' => $order,
+			'card' => $card,
+			'cardHolder' => $cardHolder,
+		];
+		
+		$autorization = $this->my_niubiz->authorization($data);
+		if ($autorization["success"]){
+			echo "Autorization result:<br/>"; 
+			print_R($autorization); 
+			echo "<br/><br/>=====================<br/><br/>";
+		}else{
+			echo $autorization["errorCode"]." - ".$autorization["errorMessage"];
+			return;
+		}
 		
 		//$this->load->view("module/obs_niubiz/index");
 	}
 	
-	public function get_session_key(){
-		$this->load->library('my_niubiz');
-		
-		$data = $this->input->post();
-		/*
-		[totalAmount] => 14797.01
-		[firstName] => Hoon Woo
-		[lastName] => Kim
-		[telephone] => 992533096
-		[additionalTelephone] => 993322119
-		[documentType] => CE
-		[documentNumber] => 000765823
-		[streetAddress] => Av. Republica de Panama 4077 Dpto 2305
-		[region] => 3165
-		[city] => 37956
-		[district] => 540285
-		[postcode] => 150141
-		[typeOfResidence] => Departamento con ascensor
-		[references] => cruce entre Av Tomas Marsano y Av Republica de Panama
-		[country] => PE
-		*/
-		
-		/* Used in 'antifraud' array */
-		$orderNo = '1234567890';//load from somewhere if you need. this is optional
-		$clientIp = '24.252.107.29';//Client IP that you have to set
-		
-		/* Used in 'dataMap' array */
-		$cardholderCity = "Lima";//set city based on $data["city"]
-		$cardholderCountry = "PE";//you can use $data["country"]
-		$cardholderAddress = $data["streetAddress"];//customer billing address. I am using $data["streetAddress"]
-		$cardholderPostalCode = $data["postcode"];//you can use $data["postcode"]
-		$cardholderState = "LIM";//set city based on $data["region"]
-		$cardholderPhoneNumber = $data["telephone"];//you can use $data["telephone"]
-		
-		/* Used for 'amount' key value */
-		$amount = $data["totalAmount"];//you can use $data["totalAmount"]
-		
-		$session_token_data = [
-			'channel' => 'web',//default
-			'antifraud' => [
-				'merchantDefineData' => [//you can add any business values here. Here i use orderNo
-					'orderNo' => $orderNo,
-					'anyKey1' => 'anyValue1',
-					'anyKey2' => 'anyValue2',
-					'anyKey3' => 'anyValue3',
-				],
-				'clientIp' => $clientIp
-			],
-			'dataMap' => [
-				'cardholderCity' => $cardholderCity,
-				'cardholderCountry' => $cardholderCountry,
-				'cardholderAddress' => $cardholderAddress,
-				'cardholderPostalCode' => $cardholderPostalCode,
-				'cardholderState' => $cardholderState,
-				'cardholderPhoneNumber' => $cardholderPhoneNumber
-			],
-			'amount' => $amount//Transaction amount
-		];
-		
-		$result = $this->my_niubiz->get_session_token($session_token_data);
-		
-		//return with order number or magento id to redirection after payment
-		$result["orderNo"] = $orderNo;
-		$result["amount"] = $amount;
-		$result["merchantId"] = $this->my_niubiz->get_merchantId();
-		
-		header('Content-Type: application/json');
-		echo json_encode($result);
-	}
 }
--- application/libraries/My_niubiz.php
+++ application/libraries/My_niubiz.php
@@ -21,7 +21,7 @@ class My_niubiz{
 	}
 	
 	public function getAccessToken(){
-		//Manual: https://desarrolladores.niubiz.com.pe/reference/get_v1-security
+		//https://desarrolladores.niubiz.com.pe/reference/get_v1-security
 		
 		$url_test = "https://apisandbox.vnforappstest.com/api.security/v1/security";
 		$url_production = "https://apiprod.vnforapps.com/api.security/v1/security";
@@ -62,8 +62,11 @@ class My_niubiz{
 		return $result;
     }
 	
-	public function getSessionToken($accessToken, $data){
-		//Manual: https://desarrolladores.niubiz.com.pe/reference/post_ecommerce-token-session-merchantid
+	public function getSessionToken($data){
+		$accessToken = $this->getAccessToken();
+		if (!$accessToken["success"]) return $accessToken;
+		
+		//https://desarrolladores.niubiz.com.pe/reference/post_ecommerce-token-session-merchantid
 		
 		$url_test = "https://apisandbox.vnforappstest.com/api.ecommerce/v2/ecommerce/token/session/".$this->merchantId;
 		$url_production = "https://apiprod.vnforapps.com/api.ecommerce/v2/ecommerce/token/session/".$this->merchantId;
@@ -80,7 +83,7 @@ class My_niubiz{
 			CURLOPT_CUSTOMREQUEST => "POST",
 			CURLOPT_POSTFIELDS => json_encode($data),
 			CURLOPT_HTTPHEADER => [
-				"Authorization: ".$accessToken,
+				"Authorization: ".$accessToken['accessToken'],
 				"accept: application/json",
 				"content-type: application/json"
 			],
@@ -124,8 +127,117 @@ class My_niubiz{
 		return $result;
 	}
 	
-	public function authorizationAPI(){
+	public function antifraud($data){
+		$accessToken = $this->getAccessToken();
+		if (!$accessToken["success"]) return $accessToken;
 		
+		//https://desarrolladores.niubiz.com.pe/reference/post_antifraud-product-merchantid
+
+		$url_test = "https://apisandbox.vnforappstest.com/api.antifraud/v1/antifraud/ecommerce/".$this->merchantId;
+		$url_production = "https://apiprod.vnforapps.com/api.antifraud/v1/antifraud/ecommerce/".$this->merchantId;
+
+		$curl = curl_init();
+
+		curl_setopt_array($curl, [
+			CURLOPT_URL => $this->is_production ? $url_production : $url_test,
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_ENCODING => "",
+			CURLOPT_MAXREDIRS => 10,
+			CURLOPT_TIMEOUT => 30,
+			CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+			CURLOPT_CUSTOMREQUEST => "POST",
+			CURLOPT_POSTFIELDS => json_encode($data),
+			CURLOPT_HTTPHEADER => [
+				"Authorization: ".$accessToken['accessToken'],
+				"accept: application/json",
+				"content-type: application/json"
+			],
+		]);
+
+		$response = curl_exec($curl);
+		$err = curl_error($curl);
+
+		curl_close($curl);
+
+		$result = ["success" => false, "header" => null, "order" => null, "token" => null, "dataMap" => null, "errorCode" => null, "errorMessage" => null];
+		
+		if ($err) echo "cURL Error #:" . $err;
+		else{
+			//response is plain text
+			if (($response === "Unauthorized access") or ($response === "Bad Request")){
+				$result["errorCode"] = "No data";
+				$result["errorMessage"] = $response." (Wrong access token.)";
+			}
+			elseif ($response === "Not Acceptable"){
+				//return in plain text. May be this is fraude case
+				$result["errorCode"] = 406;
+				$result["errorMessage"] = $response;
+			}else{
+				//sure that response is a json
+				$response = json_decode($response);
+				if (property_exists($response, "errorCode")){//error ocurred
+					//one of cases => {"errorCode":400,"errorMessage":"Token has been used before","data":{}}
+					$result["errorCode"] = $response->errorCode;
+					$result["errorMessage"] = $response->errorMessage;
+					$result["data"] = $response->data;
+				}else{
+					/* example data
+					{
+						"header":{"ecoreTransactionUUID":"e9e02c39-4e89-494b-a899-7c0ad7ab2406","ecoreTransactionDate":1721451805433,"millis":533},
+						"order":{"purchaseNumber":"1234567890","amount":1.0,"currency":"PEN"},
+						"token":{"tokenId":"B6788265FB1F4717B88265FB1F871762","expiration":1721452705362,"redirectToVbV":false,"mpi":false,"dataMap":{}},
+						"dataMap":{}
+					}
+					*/
+					$result["success"] = true;
+					$result["header"] = $response->header;
+					$result["order"] = $response->order;
+					$result["token"] = $response->token;
+					$result["dataMap"] = $response->dataMap;
+				}
+			}
+		}
+		
+		return $result;
+	}
+	
+	public function authorization($data){
+		$accessToken = $this->getAccessToken();
+		if (!$accessToken["success"]) return $accessToken;
+		
+		//https://desarrolladores.niubiz.com.pe/reference/post_authorization-ecommerce-merchantid
+
+		$url_test = "https://apisandbox.vnforappstest.com/api.authorization/v3/authorization/ecommerce/".$this->merchantId;
+		$url_production = "https://apiprod.vnforapps.com/api.authorization/v3/authorization/ecommerce/".$this->merchantId;
+
+		$curl = curl_init();
+		
+		curl_setopt_array($curl, [
+			CURLOPT_URL => $this->is_production ? $url_production : $url_test,
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_ENCODING => "",
+			CURLOPT_MAXREDIRS => 10,
+			CURLOPT_TIMEOUT => 30,
+			CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+			CURLOPT_CUSTOMREQUEST => "POST",
+			CURLOPT_POSTFIELDS => json_encode($data),
+			CURLOPT_HTTPHEADER => [
+				"Authorization: ".$accessToken['accessToken'],
+				"accept: application/json",
+				"content-type: application/json"
+			],
+		]);
+
+		$response = curl_exec($curl);
+		$err = curl_error($curl);
+
+		curl_close($curl);
+
+		if ($err) {
+		  echo "cURL Error #:" . $err;
+		} else {
+		  echo $response;
+		}
 	}
 }
 ?>
