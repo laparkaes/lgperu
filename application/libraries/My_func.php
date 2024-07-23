d7e64636b98635a129c3b01c75203e29923e120a
G-Desktop <laparkaes@gmail.com>
2024-07-22 23:19:19 -0500

    .

--- application/libraries/My_func.php
+++ application/libraries/My_func.php
@@ -278,16 +278,16 @@ class My_func{
 		$s_g = ["create_date", "close_date", "customer_department", "line_status", "order_category", "order_no", "line_no", "model_category", "model", "product_level1_name","product_level4_name", "product_level4_code", "item_type_desctiption", "currency", "unit_selling_price", "ordered_qty", "sales_amount", "bill_to_name"];
 		
 		//load all this month records
-		$w_g = ["create_date >=" => $from, "create_date <=" => $to, "line_status !=" => "Cancelled", "customer_department !=" => "Branch"];
+		$w_g = ["inventory_org" => "N4E", "create_date >=" => $from, "create_date <=" => $to, "line_status !=" => "Cancelled", "customer_department !=" => "Branch"];
 		$gerps = $this->CI->gen_m->filter_select("obs_gerp_sales_order", false, $s_g, $w_g, null, null, [["create_date", "desc"], ["close_date", "desc"]]);
 		
 		//load closed on this month
-		$w_g_ = ["create_date <" => $from, "close_date >=" => $from, "close_date <=" => $to, "line_status !=" => "Cancelled", "customer_department !=" => "Branch"];
+		$w_g_ = ["inventory_org" => "N4E", "create_date <" => $from, "close_date >=" => $from, "close_date <=" => $to, "line_status !=" => "Cancelled", "customer_department !=" => "Branch"];
 		$w_in_ = [["field" => "line_status", "values" => ["Awaiting Fulfillment", "Awaiting Shipping", "Booked", "Pending pre-billing acceptance", "Closed"]]];
 		$gerps_ = $this->CI->gen_m->filter_select("obs_gerp_sales_order", false, $s_g, $w_g_, null, $w_in_, [["create_date", "desc"], ["close_date", "desc"]]);
 		
 		//load no closed orders
-		$w_g__ = ["create_date <" => $from, "line_status !=" => "Cancelled", "customer_department !=" => "Branch"];
+		$w_g__ = ["inventory_org" => "N4E", "create_date <" => $from, "line_status !=" => "Cancelled", "customer_department !=" => "Branch"];
 		$w_in__ = [["field" => "line_status", "values" => ["Awaiting Fulfillment", "Awaiting Shipping", "Booked", "Pending pre-billing acceptance"]]];
 		$gerps__ = $this->CI->gen_m->filter_select("obs_gerp_sales_order", false, $s_g, $w_g__, null, $w_in__, [["create_date", "desc"], ["close_date", "desc"]]);
 		
