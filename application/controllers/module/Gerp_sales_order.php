6a4ddd009924e82363698ecb63ccd0816c2ee924
G-Desktop <laparkaes@gmail.com>
2024-08-15 00:22:30 -0500

    .

--- application/controllers/module/Gerp_sales_order.php
+++ application/controllers/module/Gerp_sales_order.php
@@ -62,6 +62,7 @@ class Gerp_sales_order extends CI_Controller {
 	
 	private function process(){
 		set_time_limit(0);
+		ini_set('memory_limit', '2G');
 		
 		$start_time = microtime(true);
 		
@@ -102,8 +103,9 @@ class Gerp_sales_order extends CI_Controller {
 			//db fields
 			$vars = ["bill_to_name",  "ship_to_name",  "model",  "order_no",  "line_no",  "order_type",  "line_status",  "hold_flag",  "ready_to_pick",  "pick_released",  "instock_flag",  "ordered_qty",  "unit_selling_price",  "sales_amount",  "tax_amount",  "charge_amount",  "line_total",  "list_price",  "original_list_price",  "dc_rate",  "currency",  "dfi_applicable",  "aai_applicable",  "cancel_qty",  "booked_date",  "scheduled_cancel_date",  "expire_date",  "req_arrival_date_from",  "req_arrival_date_to",  "req_ship_date",  "shipment_date",  "close_date",  "line_type",  "customer_name",  "bill_to",  "customer_department",  "ship_to",  "store_no",  "price_condition",  "payment_term",  "customer_po_no",  "customer_po_date",  "invoice_no",  "invoice_line_no",  "invoice_date",  "sales_person",  "pricing_group",  "buying_group",  "inventory_org",  "sub_inventory",  "shipping_method",  "shipment_priority",  "order_source",  "order_status",  "order_category",  "quote_date",  "quote_expire_date",  "project_code",  "comm_submission_no",  "plp_submission_no",  "bpm_request_no",  "consumer_name",  "receiver_name",  "consumer_phone_no",  "consumermobile_no",  "receiver_phone_no",  "receiver_mobile_no",  "receiver_address1",  "receiver_address2",  "receiver_address3",  "receiver_city",  "receiver_city_desc",  "receiver_county",  "receiver_postal_code",  "receiver_state",  "receiver_province",  "receiver_country",  "item_division",  "product_level1_name",  "product_level2_name",  "product_level3_name",  "product_level4_name",  "product_level4_code",  "model_category",  "item_type_desctiption",  "item_weight",  "item_cbm",  "sales_channel_high",  "sales_channel_low",  "ship_group",  "back_order_hold",  "credit_hold",  "overdue_hold",  "customer_hold",  "payterm_term_hold",  "fp_hold",  "minimum_hold",  "future_hold",  "reserve_hold",  "manual_hold",  "auto_pending_hold",  "sa_hold",  "form_hold",  "bank_collateral_hold",  "insurance_hold",  "partial_flag",  "load_hold_flag",  "inventory_reserved",  "pick_release_qty",  "long_multi_flag",  "so_sa_mapping",  "picking_remark",  "shipping_remark",  "create_employee_name",  "create_date",  "dls_interface",  "edi_customer_remark",  "sales_recognition_method",  "billing_type",  "lt_day",  "carrier_code",  "delivery_number",  "manifest_grn_no",  "warehouse_job_no",  "customer_rad",  "others_out_reason",  "ship_set_name",  "promising_txn_status",  "promised_mad",  "promised_arrival_date",  "promised_ship_date",  "initial_promised_arrival_date",  "accounting_unit",  "acd_original_warehouse",  "acd_original_wh_type",  "cnjp",  "nota_no",  "nota_date",  "so_status2",  "sbp_tax_include",  "sbp_tax_exclude",  "rrp_tax_include",  "rrp_tax_exclude",  "so_fap_flag",  "so_fap_slot_date"];
 			
-			$rows = [];
+			$rows = $date_arr = [];
 			
+			$month_now = "";
 			for($i = 2; $i <= $max_row; $i++){
 				$row = [];
 				foreach($vars as $var_i => $var){
@@ -111,51 +113,94 @@ class Gerp_sales_order extends CI_Controller {
 					if (!$row[$var]) $row[$var] = null;
 				}
 				
-				//apply trim
-				$row["order_no"] = trim($row["order_no"]);
-				$row["line_no"] = trim($row["line_no"]);
-				
-				//float_convert
-				$row["unit_selling_price"] = str_replace(",", "", $row["unit_selling_price"]);
-				$row["sales_amount"] = str_replace(",", "", $row["sales_amount"]);
-				$row["tax_amount"] = str_replace(",", "", $row["tax_amount"]);
-				$row["charge_amount"] = str_replace(",", "", $row["charge_amount"]);
-				$row["line_total"] = str_replace(",", "", $row["line_total"]);
-				$row["list_price"] = str_replace(",", "", $row["list_price"]);
-				$row["original_list_price"] = str_replace(",", "", $row["original_list_price"]);
-				$row["item_weight"] = str_replace(",", "", $row["item_weight"]);
-				$row["item_cbm"] = str_replace(",", "", $row["item_cbm"]);
-				$row["sbp_tax_include"] = str_replace(",", "", $row["sbp_tax_include"]);
-				$row["sbp_tax_exclude"] = str_replace(",", "", $row["sbp_tax_exclude"]);
-				$row["rrp_tax_include"] = str_replace(",", "", $row["rrp_tax_include"]);
-				$row["rrp_tax_exclude"] = str_replace(",", "", $row["rrp_tax_exclude"]);
-				
-				//date convert: 28-OCT-21 > 2021-10-28
-				$row["booked_date"] = $this->my_func->date_convert_4($row["booked_date"]);
-				$row["scheduled_cancel_date"] = $this->my_func->date_convert_4($row["scheduled_cancel_date"]);
-				$row["expire_date"] = $this->my_func->date_convert_4($row["expire_date"]);
-				$row["req_arrival_date_from"] = $this->my_func->date_convert_4($row["req_arrival_date_from"]);
-				$row["req_arrival_date_to"] = $this->my_func->date_convert_4($row["req_arrival_date_to"]);
-				$row["req_ship_date"] = $this->my_func->date_convert_4($row["req_ship_date"]);
-				$row["shipment_date"] = $this->my_func->date_convert_4($row["shipment_date"]);
-				$row["close_date"] = $this->my_func->date_convert_4($row["close_date"]);
-				$row["invoice_date"] = $this->my_func->date_convert_4($row["invoice_date"]);
-				$row["create_date"] = $this->my_func->date_convert_4($row["create_date"]);
-				
-				//date convert: 28-OCT-2021 > 2021-10-28
-				$row["customer_po_date"] = $this->my_func->date_convert_5($row["customer_po_date"]);
-				
-				//date convert: 2021/11/02 00:00:00 > 2021-11-02
-				$row["customer_rad"] = $this->my_func->date_convert_2($row["customer_rad"]);
-				
-				//% > float
-				$row["dc_rate"] = str_replace("%", "", $row["dc_rate"])/100;
-				
-				$rows[] = $row;
-				
-				$date_arr[] = $row["create_date"];
+				if ($row["line_status"] !== "Cancelled"){
+					//apply trim
+					$row["order_no"] = trim($row["order_no"]);
+					$row["line_no"] = trim($row["line_no"]);
+					
+					//float_convert
+					$row["unit_selling_price"] = str_replace(",", "", $row["unit_selling_price"]);
+					$row["sales_amount"] = str_replace(",", "", $row["sales_amount"]);
+					$row["tax_amount"] = str_replace(",", "", $row["tax_amount"]);
+					$row["charge_amount"] = str_replace(",", "", $row["charge_amount"]);
+					$row["line_total"] = str_replace(",", "", $row["line_total"]);
+					$row["list_price"] = str_replace(",", "", $row["list_price"]);
+					$row["original_list_price"] = str_replace(",", "", $row["original_list_price"]);
+					$row["item_weight"] = str_replace(",", "", $row["item_weight"]);
+					$row["item_cbm"] = str_replace(",", "", $row["item_cbm"]);
+					$row["sbp_tax_include"] = str_replace(",", "", $row["sbp_tax_include"]);
+					$row["sbp_tax_exclude"] = str_replace(",", "", $row["sbp_tax_exclude"]);
+					$row["rrp_tax_include"] = str_replace(",", "", $row["rrp_tax_include"]);
+					$row["rrp_tax_exclude"] = str_replace(",", "", $row["rrp_tax_exclude"]);
+					
+					//date convert: 28-OCT-21 > 2021-10-28
+					$row["booked_date"] = $this->my_func->date_convert_4($row["booked_date"]);
+					$row["scheduled_cancel_date"] = $this->my_func->date_convert_4($row["scheduled_cancel_date"]);
+					$row["expire_date"] = $this->my_func->date_convert_4($row["expire_date"]);
+					$row["req_arrival_date_from"] = $this->my_func->date_convert_4($row["req_arrival_date_from"]);
+					$row["req_arrival_date_to"] = $this->my_func->date_convert_4($row["req_arrival_date_to"]);
+					$row["req_ship_date"] = $this->my_func->date_convert_4($row["req_ship_date"]);
+					$row["shipment_date"] = $this->my_func->date_convert_4($row["shipment_date"]);
+					$row["close_date"] = $this->my_func->date_convert_4($row["close_date"]);
+					$row["invoice_date"] = $this->my_func->date_convert_4($row["invoice_date"]);
+					$row["create_date"] = $this->my_func->date_convert_4($row["create_date"]);
+					
+					//date convert: 28-OCT-2021 > 2021-10-28
+					$row["customer_po_date"] = $this->my_func->date_convert_5($row["customer_po_date"]);
+					
+					//date convert: 2021/11/02 00:00:00 > 2021-11-02
+					$row["customer_rad"] = $this->my_func->date_convert_2($row["customer_rad"]);
+					
+					//% > float
+					$row["dc_rate"] = str_replace("%", "", $row["dc_rate"])/100;
+					
+					if (!$month_now) $month_now = date("Y-m", strtotime($row["booked_date"]));
+					echo $month_now." vs ".date("Y-m", strtotime($row["booked_date"]))."<br/>";
+					if ($month_now !== date("Y-m", strtotime($row["booked_date"]))){
+						$date_arr = array_unique($date_arr);
+						sort($date_arr);
+						
+						$from = min($date_arr);
+						$to = max($date_arr);
+						
+						print_r($date_arr);
+						echo "<br/><br/><br/><br/>";
+						
+						
+						echo "Inserting ======================= <br/>";
+						echo $from." ~ ".$to." ======================= <br/>";
+						foreach($rows as $item){
+							echo $item["order_no"]." ___ ".$item["line_no"]."<br/>";
+							echo $item["booked_date"]."<br/>";
+							echo "<br/>";
+						} 
+						
+						echo "<br/><br/><br/><br/>";
+						
+						//remove
+						$this->gen_m->delete("gerp_sales_order", ["booked_date >=" => $from, "booked_date <=" => $to]);
+						
+						//insert
+						$inserted = $this->gen_m->insert_m("gerp_sales_order", $rows);
+						
+						$rows = $date_arr = [];
+						
+						break;
+					}
+					
+					$rows[] = $row;
+					$date_arr[] = $row["booked_date"];
+					
+					/*
+					if (count($rows) > 5000){
+						print_r($rows); echo "<br/><br/>==========================================<br/><br/>";
+						$rows = $date_arr = [];
+					}
+					*/	
+				}
 			}
 			
+			/*
 			$date_arr = array_unique($date_arr);
 			sort($date_arr);
 			
@@ -171,6 +216,8 @@ class Gerp_sales_order extends CI_Controller {
 			}
 			
 			$this->update_model_category();
+			*/
+			
 			
 			$msg = "Finisehd.<br/><br/>".number_Format(microtime(true) - $start_time, 2)." secs";
 		}else $msg = null;
