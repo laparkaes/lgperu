e62c9599d74c16d5bc84df9cf14324921050a254
G-LG <laparkaes@gmail.com>
2024-09-04 16:31:19 -0500

    .

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -67,9 +67,32 @@ class Dashboard extends CI_Controller {
 	}
 	
 	public function test_er(){
-		$date = date("Y-m-d"); $date = "2024-08-19";
+		//$date = date("Y-m-d"); $date = "2024-08-19";
 		
 		
+		$token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoibGdlIiwic3ViIjoibGdlIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy9yb2xlIjpbIk1hbmFnZXIiLCJTdXBlcnZpc29yIl0sIm5iZiI6MTcxODgxOTgzOSwiZXhwIjoxNzUwMzU1ODM5LCJpc3MiOiJodHRwOi8vand0YXV0aHpzcnYuYXp1cmV3ZWJzaXRlcy5uZXQiLCJhdWQiOiIwOTkxNTNjMjYyNTE0OWJjOGVjYjNlODVlMDNmMDAyMiJ9.1ejIUlAPbq8FhggDzJIhXkYrRCMli1ghC8OI2PETwZc';
+		
+		$ch = curl_init();
+		$url = 'http://serviciosweb.sbs.gob.pe/api/tipocambio/contable/28082024';
+		$headers = [
+			'Accept: application/json',
+			'Authorization: Bearer '.$token,
+		];
+		
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+		curl_setopt($ch, CURLOPT_TIMEOUT, 5);//max waiting time 5 secs
+
+		$response = curl_exec($ch);
+		
+		curl_close($ch);
+
+		$ex = json_decode($response, true);
+		
+		print_r($ex);
+		
+		/*
 		
 		$dates = $this->my_func->dates_between("2024-08-01", "2024-08-31");
 		foreach($dates as $d){
@@ -83,7 +106,7 @@ class Dashboard extends CI_Controller {
 			echo "<br/><br/>";
 		}
 		
-		
+		*/
 		
 		
 	}
