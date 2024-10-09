2e6cf3042d8baaaa3c08162db366890a29277966
G-LG <laparkaes@gmail.com>
2024-10-09 18:11:41 -0500

    .

--- application/controllers/module/Ar_exchange_rate.php
+++ application/controllers/module/Ar_exchange_rate.php
@@ -74,7 +74,7 @@ class Ar_exchange_rate extends CI_Controller {
 			$dates = $this->my_func->dates_between($date_start, $date_end);
 			foreach($dates as $i => $d){
 				if (!$this->gen_m->filter("exchange_rate", false, ["date" => $d, "currency" => "PEN"])){
-					$ex = $this->my_func->get_exchange_rate_usd($d);
+					$ex = $this->my_func->load_exchange_rate_sbs($d);
 					print_r($ex); echo "<br/>";
 					
 					/*
