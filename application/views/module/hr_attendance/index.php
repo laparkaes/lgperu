15d93c7b97c39b388452e278d97e82f04c661487
G-Desktop <laparkaes@gmail.com>
2024-10-23 00:03:12 -0500

    .

--- application/views/module/hr_attendance/index.php
+++ application/views/module/hr_attendance/index.php
@@ -44,7 +44,7 @@
 							</tr>
 						</thead>
 						<tbody>
-							<?php foreach($employees as $i => $item){ if ($item["data"]->name and ($item["summary"]["check_days"] > 0)){ ?>
+							<?php foreach($employees as $pr => $item){ if ($item["data"]->name and ($item["summary"]["check_days"] > 0)){ ?>
 							<tr class="row_emp">
 								<td>
 									<div class="search_criteria d-none"><?= $item["data"]->name." ".$item["data"]->dept." ".$item["data"]->employee_number." ".$item["data"]->ep_mail ?></div>
