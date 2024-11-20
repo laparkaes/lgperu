4d144d8dc604d0cd40b12398ecd18c58c03389cd
G-LG <laparkaes@gmail.com>
2024-11-20 10:33:27 -0500

    .

--- application/views/page/pi_listening/index.php
+++ application/views/page/pi_listening/index.php
@@ -1,5 +1,5 @@
 <div class="row">
-	<div class="col-md-10 mx-auto pt-3">
+	<div class="col-md-6 mx-auto pt-3">
 		<?php
 		$type = $msg = null;
 		if ($this->session->flashdata('success_msg')){
@@ -20,12 +20,12 @@
 			<div class="card-body">
 				<h5 class="card-title">PI - LISTENING TO YOU !!!</h5>
 				<form class="row g-3" method="POST" action="<?= base_url() ?>page/pi_listening/cpilistening">
-					<div class="col-md-6">
+					<!-- div class="col-md-6">
 						<label for="dptFrom" class="form-label">From (Department code provided by PI)</label>
 						<input type="text" class="form-control" id="dptFrom" name="dptFrom" value="<?= $this->session->flashdata('dptFrom') ?>" required>
-					</div>
-					<div class="col-md-6">
-						<label for="dptTo" class="form-label">To</label>
+					</div-->
+					<div class="col-md-12">
+						<label for="dptTo" class="form-label">Department that wishes to submit the proposal</label>
 						<select id="dptTo" name="dptTo" class="form-select" required>
 							<option value="" selected="">Choose...</option>
 							<?php foreach($dpts as $key => $item){ ?>
@@ -38,12 +38,11 @@
 						<textarea class="form-control" id="issue" name="issue" style="height: 300px" required><?= $this->session->flashdata('issue') ?></textarea>
 					</div>
 					<div class="col-md-6">
-						<label for="solution" class="form-label">Solution</label>
+						<label for="solution" class="form-label">Proposal</label>
 						<textarea class="form-control" id="solution" name="solution" style="height: 300px" required><?= $this->session->flashdata('solution') ?></textarea>
 					</div>				
-					<div class="text-center pt-5">
+					<div class="text-center pt-3">
 						<button type="submit" class="btn btn-primary">Submit</button>
-						<button type="reset" class="btn btn-secondary">Reset</button>
 					</div>
               </form>
 			</div>
