77a0285fdd1eef8fe5925e1852216f5451596f9b
G-Desktop <laparkaes@gmail.com>
2024-12-29 15:37:44 -0500

    .

--- application/views/data_upload/lgepr_sales_order/index.php
+++ application/views/data_upload/lgepr_sales_order/index.php
@@ -1,10 +1,10 @@
 <div class="d-flex justify-content-between align-items-start">
 	<div class="pagetitle">
-		<h1>LGEPR Sale Order Update</h1>
+		<h1>LGEPR Sale Order</h1>
 		<nav>
 			<ol class="breadcrumb">
 				<li class="breadcrumb-item"><a href="<?= base_url() ?>dashboard">Dashboard</a></li>
-				<li class="breadcrumb-item active">LGEPR Sale Order Update </li>
+				<li class="breadcrumb-item active">LGEPR Sale Order</li>
 			</ol>
 		</nav>
 	</div>
@@ -15,7 +15,7 @@
 			<div class="card">
 				<div class="card-body">
 					<div class="d-flex justify-content-between align-items-center">
-						<h5 class="card-title">Last 5,000 sales orders</h5>
+						<h5 class="card-title">Sales Orders</h5>
 						<form id="form_sales_order_upload">
 							<div class="input-group">
 								<a class="btn btn-success" href="<?= base_url() ?>template/lgepr_sales_order_template.xls" download="lgepr_sales_order_template"><i class="bi bi-file-earmark-spreadsheet"></i></a>
@@ -24,47 +24,48 @@
 							</div>
 						</form>
 					</div>
-					<?php foreach($sales_orders as $item){ print_r($item); ?>
-					<br/><br/>
-					<?php } ?>
-					<table class="table datatable">
+					<table class="table">
 						<thead>
 							<tr>
-								<th scope="col">Order</th>
 								<th scope="col">Status</th>
+								<th scope="col">Created</th>
+								<th scope="col">Type</th>
+								<th scope="col">Order #</th>
+								<th scope="col">Amount USD</th>
 								<th scope="col">Inventory</th>
-								<th scope="col">Bill to</th>
-								<th scope="col">Order no & Line</th>
+								<th scope="col">Division</th>
+								<th scope="col">Category</th>
 								<th scope="col">Model</th>
 								<th scope="col">Qty</th>
-								<th scope="col">Currency</th>
-								<th scope="col">Sales Amount</th>
-								<th scope="col">Created</th>
-								<th scope="col">Requested</th>
-								<th scope="col">Closed</th>
+								<th scope="col">Level 1</th>
+								<th scope="col">Level 2</th>
+								<th scope="col">Level 3</th>
+								<th scope="col">Level 4</th>
+								<th scope="col">Dept.</th>
+								<th scope="col">Bill To</th>
+								<th scope="col">Ship To</th>
 							</tr>
 						</thead>
 						<tbody>
-							<?php foreach($sales_orders as $item){ break; ?>
+							<?php foreach($sales_orders as $item){ ?>
 							<tr>
-								<td><?= $item->order_category ?></td>
 								<td><?= $item->line_status ?></td>
-								<td><?= $item->inventory_org ?></td>
-								<td><?= $item->bill_to_name ?> (<?= $item->bill_to ?>)</td>
-								<td>
-									<div><?= $item->order_no ?></div>
-									<div><?= $item->line_no ?></div>
-								</td>
-								<td>
-									<div><?= $item->model_category ?></div>
-									<div><?= $item->model ?></div>
-								</td>
-								<td><?= number_format($item->ordered_qty) ?></td>
-								<td><?= $item->currency ?></td>
-								<td><?= number_format($item->sales_amount, 2) ?></td>
 								<td><?= $item->create_date ?></td>
-								<td><?= $item->req_arrival_date_to ?></td>
-								<td><?= $item->close_date ?></td>
+								<td><?= $item->order_category ?></td>
+								<td><?= $item->order_no." ".$item->line_no ?></td>
+								<td><?= number_format($item->sales_amount_usd, 2) ?></td>
+								<td><?= $item->inventory_org ?><br/><?= $item->sub_inventory ?></td>
+								<td><?= $item->dash_division ?></td>
+								<td><?= $item->dash_category ?></td>
+								<td><?= $item->model ?></td>
+								<td><?= $item->ordered_qty ?></td>
+								<td><?= $item->product_level1_name ?></td>
+								<td><?= $item->product_level2_name ?></td>
+								<td><?= $item->product_level3_name ?></td>
+								<td><?= $item->product_level4_name ?></td>
+								<td><?= $item->customer_department ?></td>
+								<td><?= $item->bill_to_name ?><br/><?= $item->bill_to ?></td>
+								<td><?= $item->ship_to_name ?><br/><?= $item->ship_to ?></td>
 							</tr>
 							<?php } ?>
 						</tbody>
