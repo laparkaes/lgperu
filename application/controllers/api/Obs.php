35beeee462382d07355e83c3b32ba2f1efdab64a
G-LG <laparkaes@gmail.com>
2025-01-03 18:53:56 -0500

    .

--- application/controllers/api/Obs.php
+++ application/controllers/api/Obs.php
@@ -19,38 +19,57 @@ class Obs extends CI_Controller {
 	}
 	
 	public function get_company(){
-		$companies = [
-			["company" => "HS", "seq" => "a"],
-			["company" => "MS", "seq" => "b"],
-			["company" => "ES", "seq" => "c"],
-		];
+		//llamasys/local_api/get_company?key=lgepr
+		
+		if ($this->input->get("key") === "lgepr"){
+			$res = [
+				["company" => "HS", "seq" => "a"],
+				["company" => "MS", "seq" => "b"],
+				["company" => "ES", "seq" => "c"],
+			];
+		}else $res = ["Key error"];
 		
 		header('Content-Type: application/json');
-		echo json_encode($companies);
+		echo json_encode($res);
 	}
 	
 	public function get_division(){
-		$companies = [
-			["company" => "HS",	"division" => "REF",		"seq" => "a"],
-			["company" => "HS",	"division" => "Cooking",	"seq" => "b"],
-			["company" => "HS",	"division" => "Dishwasher",	"seq" => "c"],
-			["company" => "HS",	"division" => "W/M",		"seq" => "d"],
-			
-			["company" => "MS",	"division" => "LTV",			"seq" => "e"],
-			["company" => "MS",	"division" => "Audio",			"seq" => "f"],
-			["company" => "MS",	"division" => "MNT",			"seq" => "g"],
-			["company" => "MS",	"division" => "DS",				"seq" => "h"],
-			["company" => "MS",	"division" => "PC",				"seq" => "i"],
-			["company" => "MS",	"division" => "MNT Signage",	"seq" => "j"],
-			["company" => "MS",	"division" => "Commercial TV",	"seq" => "k"],
-			
-			["company" => "ES",	"division" => "RAC",		"seq" => "l"],
-			["company" => "ES",	"division" => "SAC",		"seq" => "m"],
-			["company" => "ES",	"division" => "Chiller",	"seq" => "n"],
-		];
+		//llamasys/local_api/get_division?key=lgepr
+		
+		if ($this->input->get("key") === "lgepr"){
+			$res = [
+				["company" => "HS",	"division" => "REF",		"seq" => "a"],
+				["company" => "HS",	"division" => "Cooking",	"seq" => "b"],
+				["company" => "HS",	"division" => "Dishwasher",	"seq" => "c"],
+				["company" => "HS",	"division" => "W/M",		"seq" => "d"],
+				
+				["company" => "MS",	"division" => "LTV",			"seq" => "e"],
+				["company" => "MS",	"division" => "Audio",			"seq" => "f"],
+				["company" => "MS",	"division" => "MNT",			"seq" => "g"],
+				["company" => "MS",	"division" => "DS",				"seq" => "h"],
+				["company" => "MS",	"division" => "PC",				"seq" => "i"],
+				["company" => "MS",	"division" => "MNT Signage",	"seq" => "j"],
+				["company" => "MS",	"division" => "Commercial TV",	"seq" => "k"],
+				
+				["company" => "ES",	"division" => "RAC",		"seq" => "l"],
+				["company" => "ES",	"division" => "SAC",		"seq" => "m"],
+				["company" => "ES",	"division" => "Chiller",	"seq" => "n"],
+			];
+		}else $res = ["Key error"];
 		
 		header('Content-Type: application/json');
-		echo json_encode($companies);
+		echo json_encode($res);
+	}
+
+	public function get_most_likely(){
+		//llamasys/local_api/get_most_likely?key=lgepr
+		
+		if ($this->input->get("key") === "lgepr"){
+			$res = $this->gen_m->filter("obs_most_likely", false, ["year" => date("Y"), "month" => date("m"), "division !=" => null]);
+		}else $res = ["msg" => "Error"];
+		
+		header('Content-Type: application/json');
+		echo json_encode($res);
 	}
 	
 	public function get_market_summary(){
