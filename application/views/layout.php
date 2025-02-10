d6ce5445c900198e335142ee4fe83716eac92bb9
G-LG <laparkaes@gmail.com>
2025-02-10 09:00:30 -0500

    .

--- application/views/layout.php
+++ application/views/layout.php
@@ -4,7 +4,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta content="width=device-width, initial-scale=1.0" name="viewport">
-	<title>LG Peru</title>
+	<title>LG Peru2</title>
 	<meta content="" name="description">
 	<meta content="" name="keywords">
 	<link href="<?= base_url() ?>assets/img/favicon.png" rel="icon">
