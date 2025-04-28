2202018202312c044ceea98d2cd1e4778a2f4201
G-LG <laparkaes@gmail.com>
2025-04-28 15:17:21 -0500

    .

--- application/controllers/dash/Order_status.php
+++ application/controllers/dash/Order_status.php
@@ -57,7 +57,6 @@ class Order_status extends CI_Controller {
 		];
 		
 		$data = [];
-		
 		foreach($dpts as $dpt){
 			$data[$dpt->lookup]["data"] = $cols;
 			$data[$dpt->lookup]["coms"] = [];
@@ -103,6 +102,27 @@ class Order_status extends CI_Controller {
 		$d = $this->input->get("d");
 		if (!$d) $d = date("Y-m");
 		
+		$total = [
+			"monthly_report" => 0,
+			"ml" => 0,
+			"ml_actual" => 0,
+			"po_needs" => 0,
+			"sales_projection" => 0,
+			"sales_projection_per" => 0,
+			"actual_original" => 0,
+			"actual" => 0,
+			"actual_per" => 0,
+			"expected" => 0,
+			"shipped" => 0,
+			"picking" => 0,
+			"appointment" => 0,
+			"customer" => 0,
+			"requested" => 0,
+			"no_booked" => 0,
+			"no_alloc" => 0,
+			"sales_deduction" => 0,
+		];
+		
 		//setting initial arrays
 		$rows = $this->get_dash_company_division(false);
 		$data_no_mapping = [];
@@ -129,6 +149,7 @@ class Order_status extends CI_Controller {
 				if (array_key_exists($item->customer_department, $rows)) {
 					if (array_key_exists($item->dash_company, $rows[$item->customer_department]["coms"])) {
 						if (array_key_exists($item->dash_division, $rows[$item->customer_department]["coms"][$item->dash_company]["divs"])) {
+							$total["actual_original"] += $item->order_amount_usd;
 							$rows[$item->customer_department]["data"]["actual_original"] += $item->order_amount_usd;
 							$rows[$item->customer_department]["coms"][$item->dash_company]["data"]["actual_original"] += $item->order_amount_usd;
 							$rows[$item->customer_department]["coms"][$item->dash_company]["divs"][$item->dash_division]["data"]["actual_original"] += $item->order_amount_usd;
@@ -142,6 +163,7 @@ class Order_status extends CI_Controller {
 		$dpt_deductions = ["LGEPR"];//LGEPR is unique department applying sales deduction
 		$w_sd = ["yyyy" => date("Y", strtotime($d)), "mm" => date("m", strtotime($d))];
 		
+		$sub_sd_amount = 0;
 		foreach($dpt_deductions as $dpt){
 			$dpt_sd_amount = 0;
 			
@@ -165,6 +187,7 @@ class Order_status extends CI_Controller {
 					//calculate actual
 					$rows[$dpt]["coms"][$com]["divs"][$div]["data"]["actual"] = $rows[$dpt]["coms"][$com]["divs"][$div]["data"]["actual_original"] - $div_sd_amount;
 					
+					$sub_sd_amount += $div_sd_amount;
 					$dpt_sd_amount += $div_sd_amount;
 					$com_sd_amount += $div_sd_amount;
 				}
@@ -182,6 +205,12 @@ class Order_status extends CI_Controller {
 			$rows[$dpt]["data"]["actual"] = $dpt_actual_original - $dpt_sd_amount;
 			$rows[$dpt]["data"]["sales_deduction"] = $dpt_sd_amount / $dpt_actual_original;
 		}
+		
+		$sub_actual_original = $total["actual_original"];
+
+		//calculate sales deduction % and amount of company
+		$total["actual"] = $sub_actual_original - $sub_sd_amount;
+		$total["sales_deduction"] = $sub_sd_amount / $sub_actual_original;
 		
 		//setting sales orders
 		$w = [];
@@ -215,18 +244,22 @@ class Order_status extends CI_Controller {
 					if (array_key_exists($item->dash_company, $rows[$item->customer_department]["coms"])) {
 						if (array_key_exists($item->dash_division, $rows[$item->customer_department]["coms"][$item->dash_company]["divs"])) {
 							if ($item->shipment_date){
+								$total["shipped"] += $item->sales_amount_usd;
 								$rows[$item->customer_department]["data"]["shipped"] += $item->sales_amount_usd;
 								$rows[$item->customer_department]["coms"][$item->dash_company]["data"]["shipped"] += $item->sales_amount_usd;
 								$rows[$item->customer_department]["coms"][$item->dash_company]["divs"][$item->dash_division]["data"]["shipped"] += $item->sales_amount_usd;	
 							}elseif ($item->appointment_date){
+								$total["appointment"] += $item->sales_amount_usd;
 								$rows[$item->customer_department]["data"]["appointment"] += $item->sales_amount_usd;
 								$rows[$item->customer_department]["coms"][$item->dash_company]["data"]["appointment"] += $item->sales_amount_usd;
 								$rows[$item->customer_department]["coms"][$item->dash_company]["divs"][$item->dash_division]["data"]["appointment"] += $item->sales_amount_usd;	
 							}elseif ($item->booked_date){
+								$total["requested"] += $item->sales_amount_usd;
 								$rows[$item->customer_department]["data"]["requested"] += $item->sales_amount_usd;
 								$rows[$item->customer_department]["coms"][$item->dash_company]["data"]["requested"] += $item->sales_amount_usd;
 								$rows[$item->customer_department]["coms"][$item->dash_company]["divs"][$item->dash_division]["data"]["requested"] += $item->sales_amount_usd;	
 							}else{
+								$total["no_booked"] += $item->sales_amount_usd;
 								$rows[$item->customer_department]["data"]["no_booked"] += $item->sales_amount_usd;
 								$rows[$item->customer_department]["coms"][$item->dash_company]["data"]["no_booked"] += $item->sales_amount_usd;
 								$rows[$item->customer_department]["coms"][$item->dash_company]["divs"][$item->dash_division]["data"]["no_booked"] += $item->sales_amount_usd;	
@@ -237,15 +270,7 @@ class Order_status extends CI_Controller {
 			}
 		}
 		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+		print_r($total); echo "<br/><br/>";
 		
 		/* rows debugging */
 		foreach($rows as $dpt => $dpt_item){
