472d98b5b048155e910c48cc372cd39e5863ae5f
G-Desktop <laparkaes@gmail.com>
2025-10-08 14:40:45 -0500

    .

--- application/controllers/module/Lgepr_order.php
+++ application/controllers/module/Lgepr_order.php
@@ -32,4 +32,74 @@ class Lgepr_order extends CI_Controller {
 		
 	}
 	
+	public function daily_order_closing_report(){
+		//only_multi($tablename, $fields, $where = null, $groups = null);
+		
+		$from = date("Y-m-01");
+		$to = date("Y-m-t");
+		
+		$start_date = new DateTime('first day of this month');
+		$end_date = new DateTime('last day of this month +1 day');
+		$interval = new DateInterval('P1D'); // P1D = 1 Day
+		
+		$period = new DatePeriod($start_date, $interval, $end_date);
+
+		$date_arr = [];
+		foreach ($period as $date) {
+			$date_arr[] = [$date->format('Y-m-d') => ["plan" => 0, "close" => 0]];
+		}
+		
+		print_r($date_arr);
+		
+		$rows = [
+			"LGEPR" => [
+				"HS" => [
+					
+				],
+				"MS" => [
+				
+				],
+				"ES" => [
+				
+				],
+			],
+			"Branch" => [
+			
+			],
+		];
+		
+		
+		
+		$departments = $this->gen_m->only_multi("lgepr_order", ["department"], ["dash_company !=" => null, "booked_date <=" => $from]);
+		foreach($departments as $item){
+			print_r($item); echo "<br/>";
+		}
+		
+		echo "<br/>";
+		
+		
+		$com_div = $this->gen_m->only_multi("lgepr_order", ["dash_company", "dash_division"], ["dash_company !=" => null, "booked_date <=" => $from]);
+		foreach($com_div as $item){
+			print_r($item); echo "<br/>";
+		}
+		
+		echo "<br/>";
+		
+		
+		$bill_to_names = $this->gen_m->only_multi("lgepr_order", ["bill_to_name"], ["dash_company !=" => null, "booked_date <=" => $from]);
+		foreach($bill_to_names as $item){
+			print_r($item); echo "<br/>";
+		}
+		
+		echo "<br/>";
+		
+		
+		$so_status = $this->gen_m->only_multi("lgepr_order", ["so_status"], ["dash_company !=" => null, "booked_date <=" => $from]);
+		foreach($so_status as $item){
+			print_r($item); echo "<br/>";
+		}
+		
+		echo "<br/>";
+	}
+	
 }
